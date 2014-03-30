a91bc87bee356c39b91addc12306991305beceae
Michał Górny <mgorny@gentoo.org>
2014-03-30 18:35:29 +0200

    Abstract out keyword checking into generic PackageClass.

--- gpyutils/packages.py
+++ gpyutils/packages.py
@@ -2,7 +2,35 @@
 # (c) 2013 Michał Górny <mgorny@gentoo.org>
 # Released under the terms of the 2-clause BSD license.
 
+from .util import EnumObj
+
 import sys
+
+class PackageClass(object):
+	""" Package stability class. """
+
+	class non_keyworded(EnumObj(1)):
+		""" Package with empty keywords (likely live). """
+		pass
+
+	class testing(EnumObj(2)):
+		""" Package with ~ keywords only. """
+		pass
+
+	class stable(EnumObj(3)):
+		""" Package with at least a single stable keyword. """
+		pass
+
+
+def get_package_class(pkg):
+	k = frozenset(pkg.keywords)
+	if any([x[0] not in ('~', '-') for x in k]):
+		return PackageClass.stable
+	elif k:
+		return PackageClass.testing
+	else:
+		return PackageClass.non_keyworded
+
 
 def group_packages(pkgs, key='key', verbose=True):
 	prev_key = None
