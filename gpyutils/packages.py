8942fc1a27d3bd4e7d48d157d957e4e845ec645a
Michał Górny <mgorny@gentoo.org>
2017-05-14 10:10:01 +0200

    gpy-showimpls: Output redundancy (alike showkw but including pyimpls)

--- gpyutils/packages.py
+++ gpyutils/packages.py
@@ -2,6 +2,7 @@
 # (c) 2013 Michał Górny <mgorny@gentoo.org>
 # Released under the terms of the 2-clause BSD license.
 
+from .implementations import (get_python_impls, Status)
 from .util import EnumObj
 
 import sys
@@ -46,3 +47,40 @@ def group_packages(pkgs, key='key', verbose=True):
 
 	if curr:
 		yield curr
+
+
+def find_redundant(pkgs):
+	"""
+	Find redundant packages in the group, i.e. those that have newer
+	versions with a superset of keywords and implementations.
+	"""
+	max_keywords = {}
+	max_impls = set()
+	for p in reversed(pkgs):
+		redundant = True
+
+		# live ebuilds are never redundant
+		if not p.keywords:
+			redundant = False
+
+		# first, determine non-redundancy via keywords
+		for k in p.keywords:
+			if k.startswith('~'):
+				v = 1
+				k = k[1:]
+			else:  # stable
+				v = 2
+
+			if max_keywords.get(k, 0) < v:
+				max_keywords[k] = v
+				redundant = False
+
+		# then determine non-redundancy via impls
+		impls = set(i for i in get_python_impls(p)
+				if i.status not in (Status.dead, Status.future))
+		if not max_impls.issuperset(impls):
+			redundant = False
+			max_impls.update(impls)
+
+		if redundant:
+			yield p
