5484d734ebd4289f3f0b5405c55169a962cf6339
Michał Górny <mgorny@gentoo.org>
2018-07-14 00:50:59 +0200

    Bump to 0.2.2

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ class TestCommand(Command):
 
 setup(
 		name = 'gpyutils',
-		version = '0.2.1',
+		version = '0.2.2',
 		author = 'Michał Górny',
 		author_email = 'mgorny@gentoo.org',
 		url = 'https://github.com/mgorny/gpyutils',
