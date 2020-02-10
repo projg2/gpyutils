1ebcef65fe844ce27b8aa93bad47b31770cc124c
Michał Górny <mgorny@gentoo.org>
2020-02-10 22:18:21 +0100

    Bump to 0.4.1
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ class TestCommand(Command):
 
 setup(
 		name = 'gpyutils',
-		version = '0.4',
+		version = '0.4.1',
 		author = 'Michał Górny',
 		author_email = 'mgorny@gentoo.org',
 		url = 'https://github.com/mgorny/gpyutils',
