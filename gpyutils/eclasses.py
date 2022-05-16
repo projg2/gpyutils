4c973d3aca6061f719d1b66e7f5ce9308bf9151e
Michał Górny <mgorny@gentoo.org>
2022-05-16 19:25:14 +0200

    eclasses: Replace custom EnumObj with plain enum.Enum
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- gpyutils/eclasses.py
+++ gpyutils/eclasses.py
@@ -2,28 +2,20 @@
 # (c) 2017-2022 Michał Górny <mgorny@gentoo.org>
 # Released under the terms of the 2-clause BSD license.
 
-from .util import EnumObj
-
-from gentoopm.basepm.atom import PMAtom
+import enum
 
 
-class PkgType(object):
-    """ Package sub-type. """
+@enum.unique
+class PkgType(enum.Enum):
+    """Package type."""
 
-    class python(EnumObj(4)):
-        eclass_r1 = 'python-r1'
-
-    class python_single(EnumObj(3)):
-        eclass_r1 = 'python-single-r1'
-
-    class python_any(EnumObj(1)):
-        eclass_r1 = 'python-any-r1'
-
-    all_types = (python, python_single, python_any)
+    python = "python-r1"
+    python_single = "python-single-r1"
+    python_any = "python-any-r1"
 
 
 def guess_package_type(pkg):
-    for s in PkgType.all_types:
-        if s.eclass_r1 in pkg.inherits:
+    for s in PkgType:
+        if s.value in pkg.inherits:
             return s
     return None
