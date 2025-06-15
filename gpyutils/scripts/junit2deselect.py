d9e2d03bebad7b712e86667a48c2bd4e6919ac19
Michał Górny <mgorny@gentoo.org>
2025-06-15 18:45:39 +0200

    junit2deselect: Abstract away <testcase/> processing
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- gpyutils/scripts/junit2deselect.py
+++ gpyutils/scripts/junit2deselect.py
@@ -4,34 +4,56 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 import argparse
+import dataclasses
 import os
 import sys
+import typing
 
 import lxml.etree
 
 
-def testcase_to_selector(testcase: lxml.Element) -> str:
-    classname = testcase.get("classname")
-    name = testcase.get("name")
-    if classname is None or name is None:
-        raise RuntimeError(
-            "classname or name is missing on <testcase/>, "
-            "invalid junit xml file")
-    path = testcase.get("file")
-    if path is None:
-        raise RuntimeError(
-            "path is missing on <testcase/>, not an xunit1 format file")
+@dataclasses.dataclass(frozen=True)
+class TestCase:
+    class_ref: str
+    name: str
+    path: str
 
-    classname_from_path = path.replace(os.sep, ".").rstrip(".py")
-    # global test function
-    if classname_from_path == classname:
-        return f"{path}::{name}"
+    @classmethod
+    def from_xml(cls, testcase: lxml.Element) -> typing.Self:
+        classname = testcase.get("classname")
+        name = testcase.get("name")
+        if classname is None or name is None:
+            raise RuntimeError(
+                "classname or name is missing on <testcase/>, "
+                "invalid junit xml file")
+        path = testcase.get("file")
+        if path is None:
+            raise RuntimeError(
+                "path is missing on <testcase/>, not an xunit1 format file")
+        return cls(class_ref=classname, name=name, path=path)
 
-    # test in class
-    actual_classname = classname.removeprefix(f"{classname_from_path}.")
-    if actual_classname == classname:
-        raise RuntimeError(f"{classname=} does not match {path=}")
-    return f"{path}::{actual_classname}::{name}"
+    @property
+    def import_name(self) -> str:
+        """Convert path to import name"""
+        return self.path.replace(os.sep, ".").rstrip(".pyx")
+
+    @property
+    def class_name(self) -> str | None:
+        """Pure class name or None if global function"""
+        stripped = self.class_ref.removeprefix(self.import_name)
+        if not stripped:
+            return None
+        if stripped.startswith("."):
+            return stripped.removeprefix(".")
+        raise RuntimeError(f"{self.class_ref=} does not match {self.path=}")
+
+    @property
+    def pytest_selector(self) -> str:
+        class_name = self.class_name
+        if class_name is not None:
+            return f"{self.path}::{class_name}::{self.name}"
+        else:
+            return f"{self.path}::{self.name}"
 
 
 def main(prog_name: str, *argv: str) -> int:
@@ -42,9 +64,14 @@ def main(prog_name: str, *argv: str) -> int:
                       help="junit xml file to process")
     args = argp.parse_args(argv)
 
+    failing_tests = [
+        TestCase.from_xml(testcase)
+        for testcase in args.xml.xpath("//testcase[failure|error]")
+    ]
+
     print("EPYTEST_DESELECT=(")
-    for testcase in args.xml.xpath("//testcase[failure|error]"):
-        print(f"\t{testcase_to_selector(testcase)}")
+    for test in failing_tests:
+        print(f"\t{test.pytest_selector}")
     print(")")
 
     return 0
